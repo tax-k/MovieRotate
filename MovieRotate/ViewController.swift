9d3dfcf8b91eac1d8834ed9570e50069f51dedfd
tax_k <taxfree9090@gmail.com>
2018-10-05 17:35:12 +0900

    First Setting

--- MovieRotate/ViewController.swift
+++ MovieRotate/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func goAction(_ sender: Any) {
+        let nextView = storyboard?.instantiateViewController(withIdentifier: "rotate")
+        navigationController?.pushViewController(nextView!, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
